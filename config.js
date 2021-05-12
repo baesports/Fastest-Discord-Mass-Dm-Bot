bfdc412545162c697f5247bd32bdfdb6a719eaac
baesports <82220862+baesports@users.noreply.github.com>
2021-05-12 23:14:22 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 {
-  "prefix": "prefix", 
-  "token": "token",
-  "Id": "owner id"
+  "prefix": "%", 
+  "token": "ODQwMjI1OTgxNTU4MTYxNDA5.YJVHbA.Cx2ltaHmx2WE40RYDihmyggVqG8",
+  "Id": "743816101670158376"
 }
